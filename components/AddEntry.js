da480505fb253e101b3f211a8a60da1615154eff
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2017-09-24 00:22:30 +0700

    Connect redux to projects

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import { View, TouchableOpacity, Text } from 'react-native'
-import { getMetricMetaInfo, timeToString } from '../utils/helpers'
+import { getMetricMetaInfo, timeToString, getDailyReminderValue } from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader'
 import { Ionicons } from '@expo/vector-icons'
 import TextButton from './TextButton'
 import { submitEntry, removeEntry } from '../utils/api'
+import { connect } from 'react-redux'
+import { addEntry } from '../actions'
 
 const SubmitBtn = ({ onPress }) => {
 	return (
@@ -61,7 +63,9 @@ class AddEntry extends Component {
 		const key = timeToString()
 		const entry = this.state
 
-		// TODO: Update Redux
+		this.props.dispatch(addEntry({
+			[key]: entry
+		}))
 
 		this.setState(() => ({
 			run: 0,
@@ -81,7 +85,9 @@ class AddEntry extends Component {
 	reset = () => {
 		const key = timeToString()
 
-		// TODO: Update Redux
+		this.props.dispatch(addEntry({
+			[key]: getDailyReminderValue()
+		}))
 
 		// TODO: Navigate to home
 
@@ -132,4 +138,12 @@ class AddEntry extends Component {
 	}
 }
 
-export default AddEntry
+function mapStateToProps(state) {
+	const key = timeToString()
+
+	return {
+		alreadyLogged: state[key] && typeof state[key].today === 'undefined'
+	}
+}
+
+export default connect(mapStateToProps)(AddEntry)
