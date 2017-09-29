b0677ff2ce1c5fd7901de6052287ee3c7b36f68a
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2017-09-29 23:57:53 +0700

    Add location

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import { purple, white } from './utils/colors'
 import { FontAwesome, Ionicons } from '@expo/vector-icons'
 import { Constants } from 'expo'
 import EntryDetail from './components/EntryDetail'
+import Live from './components/Live'
 
 function UdaciStatusBar({ backgroundColor, ...props }) {
   return (
@@ -33,6 +34,15 @@ const Tabs = TabNavigator(
       navigationOptions: {
         tabBarLabel: 'Add Entry',
         tabBarIcon: ({ tintColor }) => <Ionicons name="plus-square" size={30} color={tintColor} />
+      }
+    },
+    Live: {
+      screen: Live,
+      navigationOptions: {
+        tabBarLabel: 'Live',
+        tabBarIcon: ({ tintColor }) => (
+          <Ionicons name="ios-speedometer" size={30} color={tintColor} />
+        )
       }
     }
   },
